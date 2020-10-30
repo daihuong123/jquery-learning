8d811d231f1a033f8d41bb52e82d571fc4df4321
daihuong123 <66808338+daihuong123@users.noreply.github.com>
2020-10-30 19:19:00 +0700

    e lam bai tinh tong xog ma e dang coi làm cách nhập n

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,44 @@
+// var w = confirm("trapezoida");
+// if (w == true) {
+//   var i, j;
+//   var n = 20;
+//   for (i = 15; i < n; ++i) {
+//     for (j = 0; j <= i; ++j) {
+//       document.write("*");
+//     }
+//     document.write("<br>");
+//   }
+//   document.write("<br>");
+// } else {
+//   var i, j;
+//   var n = 20;
+//   for (i = 15; i < n; ++i) {
+//     for (j = 0; j <= n - i; j++) {
+//       document.write("&nbsp&nbsp");
+//     }
+//     for (j = 1; j <= 2 * i - 1; j++) {
+//       document.write("*");
+//     }
+//     document.write("<br>");
+//   }
+// }
 
+;
 
-var w = confirm("trapezoida");
-if (w == true) {
-  var i, j;
-  var n = 20;
-  for (i = 15; i < n; ++i) {
-    for (j = 0; j <= i; ++j) {
-      document.write("*");
-    }
-    document.write("<br>");
-  }
-  document.write("<br>");
-} else {
-  var i, j;
-  var n = 20;
-  for (i = 15; i < n; ++i) {
-    for (j = 0; j <= n - i; j++) {
-      document.write("&nbsp&nbsp");
-    }
-    for (j = 1; j <= 2 * i - 1; j++) {
-      document.write("*");
-    }
-    document.write("<br>");
-  }
+ function aumm() {
+  // var n = 3 ;
+var n= document.getElementById("sum").nodeValue() ;
+
+  var sum = 0;
+  for (var i = 0; i <= n; i++) 
+    sum += i;
+    return sum;
+  
+  // document.getElementById("#idsum").innerHTML = "asdasd" + "<br>" + sum;
+  
 }
+document.write("Sum: " + aumm())
+// 
+// function main(){
 
-
+// }
