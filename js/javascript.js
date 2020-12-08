c78a74ec9d0fb9225347b090c2cdb271014af2b9
daihuong123 <66808338+daihuong123@users.noreply.github.com>
2020-12-08 19:02:42 +0700

    em lam xog r

--- js/javascript.js
+++ js/javascript.js
@@ -1,103 +1,65 @@
-// var w = confirm("trapezoida");
-// if (w == true) {
-//   var i, j;
-//   var n = 20;
-//   for (i = 15; i < n; ++i) {
-//     for (j = 0; j <= i; ++j) {
-//       document.write("*");
-//     }
-//     document.write("<br>");
-//   }
-//   document.write("<br>");
-// } else {
-//   var i, j;
-//   var n = 20;
-//   for (i = 15; i < n; ++i) {
-//     for (j = 0; j <= n - i; j++) {
-//       document.write("&nbsp&nbsp");
-//     }
-//     for (j = 1; j <= 2 * i - 1; j++) {
-//       document.write("*");
-//     }
-//     document.write("<br>");
-//   }
-// }
+const dayStart = "07:30"; //7:30, '35: 1h:20p' |7:45
+//9:20, '79: 1h:19p'
+//9:10, '10: 0h:10p'
 
-// function aumm(n) {
-//   // var n = document.getElementById("sum").value;
-//   var sum = 0;
+const dayEnd = "17:45"; //17:10, '20: 0h:20p'
+//17:30, '40: oh:40p'
 
-//   for (var i = 0; i <= n; i++) sum += i;
+function scheduleMeeting(startTime, durationMinutes) {
+  debugger
+  let timehstart = dayStart.split(":")[0];
+  let timemstart = dayStart.split(":")[1];
+  let timeh = dayEnd.split(":")[0];
+  let timem = dayEnd.split(":")[1];
+  let starth = startTime.split(":")[0];
+  let startm = startTime.split(":")[1];
 
-//   return sum;
-
-//   // document.getElementById("idsum").innerHTML = "asdasd" + "<br>" + sum;
-// }
-
-// // document.write("Sum: " + aumm())
-
-// //
-// function main() {
-//   var n = document.getElementById("sum").value;
-//   // document.write("Sum: " + aumm(n));
-//   document.getElementById("idsum").innerHTML = "Sum: "  + aumm(n);
-// }
-function tinhtb() {
-  var t = document.getElementById("idtoan");
-  var v = document.getElementById("idvan");
-// if (t.value == "" && v.value == "") {
-//   alert("enter");
-// } else if (t.value == "") {
-//   alert("chua nhap diem toan");
-// } else if (v.value == "") {
-//   alert("chua nhap diem van");
-// } else if (isNaN(t.value) && isNaN(v.value)) {
-//   alert("nhap ki tu la so lai");
-// } else if (isNaN(t.value)) {
-//   alert("nhap ksNaN(t.value)i tu la so lai");
-// } else if (isNaN(v.value)) {
-//   alert("nhap ki tu la so lai");
-// }
-  switch(t.value || v.value){
-
-    case "":
-      alert("chua nhap diem");
-      break;
-      
-   
- case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": case "10":
-    document.getElementById("iddtb").innerHTML = dtb;
-      break;
-
-
-    default: {
-      alert("nhap ki tu la so lai hoac duoi 10 ");
+  let duh = Math.floor(durationMinutes / 60);
+  let dum = durationMinutes % 60;
+  let duplusstartm = dum + Number(timem);
+  let dmhour = Math.floor(duplusstartm / 60);
+  if(Number(timeh)==Number(starth)){
+    if((Number(dmhour)+Number(timeh))>Number(timeh)){
+      alert("false")
     }
   }
+  else if (Number(timehstart)==Number(starth)){
+    if((Number(dmhour)+Number(timehstart))>Number(timehstart)){
+      alert("true")
+    }
+  }
+  else if (
+    Number(starth) + duh == Number(timehstart) ||
+    Number(starth) + duh == Number(timeh)
+  ) {
+    if (
+      Number(timemstart) <= Number(startm) + dum &&
+      Number(timem) >= Number(startm) + dum
+    ) {
+      alert("true");
+    }
+  } else if ((duh + Number(starth) >= timehstart)&&(duh + Number(starth) <= timeh)) {
+    alert("true");
+  } else if (Number(starth) + timeh <= timeh) {
+    alert("true");
+  }
 
-  var dtb = document.getElementById("iddtb");
-  dtb = (parseInt(t.value) + parseInt(v.value)) / 2;
+  else if (Number(timeh) + Number(dmhour) <= Number(timeh)) {
+    alert("true");
+  } 
+  
+  else {
+    alert("false");
+  }
+  
 
-  document.getElementById("iddtb").innerHTML = dtb;
+  
 }
-// function switchcase() {
-//   var a = document.getElementById("test");
-//   switch (a.value)
-//    {
-//     case "0": 
-//     case "1": 
-//     case "2": 
-//     case "3": 
-//     case "4": 
-//     case "5": 
-//     case "6": 
-//     case "7": 
-//     case "8": 
-//     case "9": 
-//       document.getElementById("idp").innerHTML = a.value;
-//       break;
-//     default: {
-//       alert("khong phai so");
-//     }
-//   }
-// }
+
+scheduleMeeting("07:30", "35");//t
+scheduleMeeting("07:30", "80");//t
+scheduleMeeting("17:50", "20");//f
+scheduleMeeting("17:30", "40");//f
+scheduleMeeting("9:20", "79");//t
+scheduleMeeting("18:30","20");//f
+scheduleMeeting("17:35", "15");//f
