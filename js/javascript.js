ab7cb0c06534b38b71456f42feaf04bc8af39256
daihuong123 <66808338+daihuong123@users.noreply.github.com>
2020-12-08 19:03:25 +0700

    em lam xog roi

--- js/javascript.js
+++ js/javascript.js
@@ -6,7 +6,7 @@ const dayEnd = "17:45"; //17:10, '20: 0h:20p'
 //17:30, '40: oh:40p'
 
 function scheduleMeeting(startTime, durationMinutes) {
-  debugger
+  debugger;
   let timehstart = dayStart.split(":")[0];
   let timemstart = dayStart.split(":")[1];
   let timeh = dayEnd.split(":")[0];
@@ -18,17 +18,15 @@ function scheduleMeeting(startTime, durationMinutes) {
   let dum = durationMinutes % 60;
   let duplusstartm = dum + Number(timem);
   let dmhour = Math.floor(duplusstartm / 60);
-  if(Number(timeh)==Number(starth)){
-    if((Number(dmhour)+Number(timeh))>Number(timeh)){
-      alert("false")
+  if (Number(timeh) == Number(starth)) {
+    if (Number(dmhour) + Number(timeh) > Number(timeh)) {
+      alert("false");
     }
-  }
-  else if (Number(timehstart)==Number(starth)){
-    if((Number(dmhour)+Number(timehstart))>Number(timehstart)){
-      alert("true")
+  } else if (Number(timehstart) == Number(starth)) {
+    if (Number(dmhour) + Number(timehstart) > Number(timehstart)) {
+      alert("true");
     }
-  }
-  else if (
+  } else if (
     Number(starth) + duh == Number(timehstart) ||
     Number(starth) + duh == Number(timeh)
   ) {
@@ -38,28 +36,24 @@ function scheduleMeeting(startTime, durationMinutes) {
     ) {
       alert("true");
     }
-  } else if ((duh + Number(starth) >= timehstart)&&(duh + Number(starth) <= timeh)) {
+  } else if (
+    duh + Number(starth) >= timehstart &&
+    duh + Number(starth) <= timeh
+  ) {
     alert("true");
   } else if (Number(starth) + timeh <= timeh) {
     alert("true");
-  }
-
-  else if (Number(timeh) + Number(dmhour) <= Number(timeh)) {
+  } else if (Number(timeh) + Number(dmhour) <= Number(timeh)) {
     alert("true");
-  } 
-  
-  else {
+  } else {
     alert("false");
   }
-  
-
-  
 }
 
-scheduleMeeting("07:30", "35");//t
-scheduleMeeting("07:30", "80");//t
-scheduleMeeting("17:50", "20");//f
-scheduleMeeting("17:30", "40");//f
-scheduleMeeting("9:20", "79");//t
-scheduleMeeting("18:30","20");//f
-scheduleMeeting("17:35", "15");//f
+scheduleMeeting("07:30", "35"); //t
+scheduleMeeting("07:30", "80"); //t
+scheduleMeeting("17:50", "20"); //f
+scheduleMeeting("17:30", "40"); //f
+scheduleMeeting("9:20", "79"); //t
+scheduleMeeting("18:30", "20"); //f
+scheduleMeeting("17:35", "15"); //f
